6ddd20584441601d19cb00cd8252d4e42765fa66
freelancer-abdulfaizan <af95537@gmail.com>
2021-04-03 17:21:51 +0500

    d

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // vue.config.js file to be place in the root of your repository
 
-module.exports = {
+module.exports = {    
+    baseUrl: '',
     publicPath: process.env.NODE_ENV === 'production'
       ? '/quiz_app/'
       : '/'
